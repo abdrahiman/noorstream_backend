78d4e9a809eb59e0567e13381b3c6568418313fb
abderahim <abderrahimaneddam1@gmail.com>
2023-11-07 17:01:23 +0100

    starting the search

--- utils/providers/witanime.ts
+++ utils/providers/witanime.ts
@@ -1,3 +1,25 @@
+import axios from "axios";
+import { load } from "cheerio";
+
+interface IAnime {
+    title: string;
+}
+
 export class WitAnime {
-    getHomePage() {}
+    mainUrl: string;
+    name: string;
+    constructor() {
+        this.mainUrl = "https://witanime.pics/";
+        this.name = "witanime";
+    }
+    async getHomePage() {
+        let res = await axios.get(this.mainUrl);
+        let $ = load(res.data);
+        //start scraping
+    }
+    async getSearch(query: string) {
+        const q = query.replace(/ /g, "%20");
+        let res = await axios.get(this.mainUrl + "?search_param=animes&s=" + q);
+        const result: IAnime[] = [];
+    }
 }
