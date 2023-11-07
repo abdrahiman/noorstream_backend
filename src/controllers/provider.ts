89c42d1356cf75ed47f960ce7c39b9002bb6d24a
abderahim <abderrahimaneddam1@gmail.com>
2023-11-07 17:09:32 +0100

    change routes

--- src/controllers/provider.ts
+++ src/controllers/provider.ts
@@ -1,36 +1,38 @@
 import { Request, Response } from "express";
+import { WitAnime } from "../../utils/providers/witanime";
 
 let getHomePage = async (req: Request, res: Response) => {
-  try {
-    // get all the latest anime episodes from the website then return it to the user
-    
-  } catch (err: any) {
-    return null;
-  }
+    try {
+        // get all the latest anime episodes from the website then return it to the user
+        let data = WitAnime.getHomePage();
+        return data;
+    } catch (err: any) {
+        return null;
+    }
 };
 
 let getEpisode = async (req: Request, res: Response) => {
-  try {
-  } catch (err: any) {
-    return null;
-  }
+    try {
+    } catch (err: any) {
+        return null;
+    }
 };
 let getSeason = async (req: Request, res: Response) => {
-  try {
-  } catch (err: any) {
-    return null;
-  }
+    try {
+    } catch (err: any) {
+        return null;
+    }
 };
 let getSearch = async (req: Request, res: Response) => {
-  try {
-  } catch (err: any) {
-    return null;
-  }
+    try {
+    } catch (err: any) {
+        return null;
+    }
 };
 let SearchAll = async (req: Request, res: Response) => {
-  try {
-  } catch (err: any) {
-    return null;
-  }
+    try {
+    } catch (err: any) {
+        return null;
+    }
 };
 export { getHomePage, getEpisode, getSearch, getSeason, SearchAll };
