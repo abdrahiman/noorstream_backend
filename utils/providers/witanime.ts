7246fbc7e94d4dd3610fd15d6b6ff76e34a7b6ef
abderahim <108197963+iabdrahim@users.noreply.github.com>
2023-11-07 16:46:44 +0100

    Update witanime.ts

--- utils/providers/witanime.ts
+++ utils/providers/witanime.ts
@@ -1,8 +1,16 @@
-import cheerio from 'cheerio'
+import axios from "axios";
+import { load } from "cheerio";
 
 export class WitAnime {
-    getHomePage() {
-        
+    mainUrl: string;
+    name: string;
+    constructor() {
+        this.mainUrl = "https://witanime.pics/";
+        this.name = "witanime";
     }
-
+    async getHomePage() {
+        let res = await axios.get(this.mainUrl);
+        let $ = load(res.data);
+        //start scraping
+    }
 }
