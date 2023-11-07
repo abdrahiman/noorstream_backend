d2f033e4cb338897769e784606e47946eb421625
abderahim <abderrahimaneddam1@gmail.com>
2023-11-07 16:04:54 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,8 @@ const corsOptions = {
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
 };
-app.use(cors(corsOptions)); // Use this after the variable declaration
+
+app.use(cors(corsOptions)); // Use this fter the variable declaration
 
 app.use("/data", dataRouter);
 
