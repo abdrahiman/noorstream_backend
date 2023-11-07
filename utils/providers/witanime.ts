42c2419f852003159425d8bb6854dcaf8887b81a
abderahim <abderrahimaneddam1@gmail.com>
2023-11-07 16:43:17 +0100

    adding properties to the class

--- utils/providers/witanime.ts
+++ utils/providers/witanime.ts
@@ -1,3 +1,15 @@
+import axios from "axios";
+import { load } from "cheerio";
+
 export class WitAnime {
-    getHomePage() {}
+    mainUrl: string;
+    name: string;
+    constructor() {
+        this.mainUrl = "https://witanime.pics/";
+        this.name = "witanime";
+    }
+    async getHomePage() {
+        let res = await axios.get(this.mainUrl);
+        let $ = load(res.data);
+    }
 }
