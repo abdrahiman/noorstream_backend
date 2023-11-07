7abc64761d1891e2a57f292946716146bd62986b
abderahim <abderrahimaneddam1@gmail.com>
2023-11-07 16:07:58 +0100

    commit

--- src/routes/data.ts
+++ src/routes/data.ts
@@ -6,19 +6,9 @@ import {
     SearchAll,
     getSearch,
 } from "../controllers/provider";
-import { providers } from "../../utils/providers/list";
 
 let router = express.Router();
-router.get("/", (req, res) => {
-    return res
-        .status(200)
-        .send({
-            providers: providers.map((el) => ({
-                name: el.name,
-                url: new el.class().mainUrl,
-            })),
-        });
-});
+
 router.get("/search", SearchAll);
 router.get("/:provider/search", getSearch);
 router.get("/:provider/episode", getEpisode);
